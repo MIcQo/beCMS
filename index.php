13385e190fa9b9d31fc4648e9f9ce0f85c9b1da6
MIcQo <micqogamer@gmail.com>
2016-01-30 02:26:02 +0100

    Initial commit

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  include_once('libs/loader.php');
-  // Load application
-  beCMS\beCMS::init();
-  // Build base HTML from default theme
-  beCMS\beCMS::buildHTML();
-  // Render a final HTML
-  beCMS\beCMS::render();
-?>
